9b8d8436443b9a83068f886267cad7f219ef71f8
xcyxiner <593091948@qq.com>
2018-01-28 01:49:56 +0000

    docker容器加入网络设置

--- start.sh
+++ start.sh
@@ -1,13 +1,14 @@
+tmpnetworkname="testwifi"
 tmpcontainername="mysql1"
 docker-compose up -d  $tmpcontainername
 tmpcontainerName=$(docker-compose ps | grep $tmpcontainername | awk '{print $1}')
-tmpnetworkIp=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' $tmpcontainerName)
+tmpnetworkIp=$(docker inspect --format '{{ .NetworkSettings.Networks.'$tmpnetworkname'.IPAddress }}' $tmpcontainerName)
 ./wait-for-it.sh $tmpnetworkIp:3306 -- echo "$tmpcontainername is start  success"
 
 tmpcontainername="mysql2"
 docker-compose up -d $tmpcontainername
 tmpcontainerName=$(docker-compose ps | grep $tmpcontainername | awk '{print $1}')
-tmpnetworkIp=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' $tmpcontainerName)
+tmpnetworkIp=$(docker inspect --format '{{ .NetworkSettings.Networks.'$tmpnetworkname'.IPAddress }}' $tmpcontainerName)
 ./wait-for-it.sh $tmpnetworkIp:3306 -- echo "$tmpcontainername is start  success"
 
 
